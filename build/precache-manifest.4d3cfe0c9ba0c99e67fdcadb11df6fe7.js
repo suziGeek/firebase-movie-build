9bc0064afe0493cc01d49ea717a3a291cf180ae5
Suzanne <46833370+suziGeek@users.noreply.github.com>
2020-03-08 21:57:30 -0400

    Add files via upload

---
+++ build/precache-manifest.4d3cfe0c9ba0c99e67fdcadb11df6fe7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d848302aa8c30a143c5ec363dd039b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2befd4f7651759ac902a",
+    "url": "/static/css/main.02cb95e1.chunk.css"
+  },
+  {
+    "revision": "fbf7e4a9d43764c5f704",
+    "url": "/static/js/2.6f7b806a.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/static/js/2.6f7b806a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2befd4f7651759ac902a",
+    "url": "/static/js/main.43d7d77a.chunk.js"
+  },
+  {
+    "revision": "cc1657207ab9ec14b56d",
+    "url": "/static/js/runtime-main.046c8ef5.js"
+  }
+]);
